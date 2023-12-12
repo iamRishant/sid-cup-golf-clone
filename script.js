30ed918e84744487b3853124f7a6505807a9e0b5
Rishant Rana <kiturana378@gmail.com>
2023-12-12 12:42:46 +0530

    page 2,3,4 complete

--- script.js
+++ script.js
@@ -37,3 +37,91 @@ gsap.to("#main",{
         scrub:2
     }
 })
+
+// nav bar ke elements par jab mouse jaega then uska piche ka round div ke saath cher chaar
+
+var h4All=document.querySelectorAll("#nav h4");// nav ke andr ka sara h4
+h4All.forEach((elem)=>{
+    elem.addEventListener("mouseenter",()=>{
+        crsr.style.scale=3;
+        crsr.style.border="1px solid #fff"
+        crsr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave",()=>{
+        crsr.style.scale=1;
+        crsr.style.border="0px solid ##95C11E"
+        crsr.style.backgroundColor="#95C11E"
+    })
+})
+
+gsap.from("#about-us img,#about-us-in",{
+    // x:50,
+    // yha x and y se mtlb hai ki element y +50 unit se aaega(niche se agar - hota to upar se aata) to its normal position and opacity 0se mtlb ki initially gyb hoga
+    y:50,
+    opacity:0,
+    duration:3,
+    // stagger:2//stagger lgane se ek ek krke aaega
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        // start and end tell where to start and where to end use markers true to check start and end values
+        start:"top 60%",
+        end:"top 55%",
+        scrub:3,
+    }
+})
+// card par lgane se 3d effect (rotate 3d wala effect chala jaa rha)
+// gsap.from(".card",{
+//     y:50,
+//     opacity:0,
+//     duration:2,
+//     // scale:0.8,
+//     // stagger:2//stagger lgane se ek ek krke aaega
+//     // scrollTrigger:{
+//     //     trigger:".card",
+//     //     scroller:"body",
+//     //     marker:true,
+//     //     start:"top 60%",
+//     //     end:"top 55%",
+//     //     scrub:3,
+//     // }
+// })
+
+gsap.from("#colon1",{
+    x:-70,
+    y:-70,
+    // duration:3,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 45%",
+        end:"top 30%",
+        scrub:2,
+    }
+})
+gsap.from("#colon2",{
+    x:+70,
+    y:+70,
+    // duration:3,
+    scrollTrigger:{
+        // yha trigger coln1 isiliye diye ki we want ki ek hi trigger element se dono trigger ho
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 45%",
+        end:"top 30%",
+        scrub:2,
+    }
+})
+gsap.from("#page4 h2",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4",
+        scroller:"body",
+        start:"top 72%",
+        end:"top 70%",
+        scrub:2,
+    }
+})
